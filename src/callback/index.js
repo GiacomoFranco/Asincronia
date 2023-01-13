04e00d36fc6e7ceaaf231873517e029c696bc585
Santy <passiflora.freelance@gmail.com>
2023-01-13 15:45:05 -0500

    ⚗️ Experimentando con callbacks y setTimeOut

---
+++ src/callback/index.js
@@ -0,0 +1,34 @@
+function sum(num1, num2) {
+  return num1 + num2;
+}
+function rest(num1, num2) {
+  return num1 - num2;
+}
+function mult(num1, num2) {
+  return num1 * num2;
+}
+function div(num1, num2) {
+  return num1 / num2;
+}
+
+function calc(num1, num2, callback) {
+  return callback(num1, num2);
+}
+
+console.log(calc(2, 2, mult));
+
+setTimeout(greetings, 1000, "Santy");
+
+function greetings(name) {
+  console.log(`Hí out there 👋, I'm ${name}`);
+}
+
+export function execCallback(callback, time) {
+    setTimeout(callback, time, time)
+  }
+  
+  function logSomething(time) {
+    console.log(`I'm logging something in console :) and it last ${time} miliseconds`)
+  }
+  
+execCallback(logSomething, 2000)
