c22745c7bc97c02b37a73e949f0329edeba0f4ad
Santy <passiflora.freelance@gmail.com>
2023-01-14 23:24:03 -0500

    ♻️ Añadiendo callbacks anidados

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,28 +1,30 @@
-const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-const API = "https://pokeapi.co/api/v2";
+const XMLHttpsRequest = require("xmlhttprequest").XMLHttpRequest;
+const API = "  https://api.escuelajs.co/api/v1";
 
 function fetchData(urlApi, callback) {
-
-  let xhttp = new XMLHttpRequest();
-
-  xhttp.open("GET", urlApi, true);
-
-  xhttp.onreadystatechange = function () {
-    if (xhttp.readyState === 4) {
-      if (xhttp.status === 200) {
-        callback(null, JSON.parse(xhttp.responseText));
-      }else {
-        const error = new Error(urlApi);
-        callback(error, null);
-      }
-      
+  const xhttp = new XMLHttpsRequest();
+  xhttp.open('GET', urlApi,true);
+  xhttp.onreadystatechange = function (e){
+    if(xhttp.readyState === 4){
+        if (xhttp.status === 200) {
+            callback(null, JSON.parse(xhttp.responseText))
+        }
+        else{
+            const error = new Error(urlApi)
+            callback(error, null)
+        }
     }
-  };
-
-  xhttp.send()
+  }
+  xhttp.send();
 }
 
-fetchData(`${API}/pokemon`, function (error1, data1){
-    if (error1) {console.log(error1)}
-    console.log(data1)
+fetchData(`${API}/products`, function (error, respuesta){
+    if(error) console.log(error);
+    fetchData(`${API}/products/${respuesta[0].id}`, function (error2, respuesta2){
+        if (error2) console.log(error2)
+        console.log(respuesta[0]);
+        console.log(respuesta2.title);
+    })
 })
+
+
