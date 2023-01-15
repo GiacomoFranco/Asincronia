8aa743580f25273e9c806ddecfc7dfed900e68f1
Santy <passiflora.freelance@gmail.com>
2023-01-15 10:50:06 -0500

    ✅ Añadiendo un tercer callback

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,5 +1,5 @@
 const XMLHttpsRequest = require("xmlhttprequest").XMLHttpRequest;
-const API = "  https://api.escuelajs.co/api/v1";
+const API = "https://api.escuelajs.co/api/v1";
 
 function fetchData(urlApi, callback) {
   const xhttp = new XMLHttpsRequest();
@@ -22,8 +22,13 @@ fetchData(`${API}/products`, function (error, respuesta){
     if(error) console.log(error);
     fetchData(`${API}/products/${respuesta[0].id}`, function (error2, respuesta2){
         if (error2) console.log(error2)
-        console.log(respuesta[0]);
-        console.log(respuesta2.title);
+        fetchData(`${API}/categories/${respuesta2.category.id}`, function(error3, respuesta3){
+            if (error3) console.log(error3);
+
+            console.log(respuesta[0]);
+            console.log(respuesta2);
+            console.log(respuesta3.name);
+        })
     })
 })
 
